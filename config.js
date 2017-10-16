c1048a55be2b5c72510adda82f46481be3114736
hukai <a532809319>
2017-10-16 15:43:25 +0800

    啊

--- config.js
+++ config.js
@@ -2,4 +2,5 @@
 module.exports.userdb_url = 'mongodb://localhost/passport'
 module.exports.devent_url = 'mongodb://localhost/devent'
 module.exports.dalert_url = 'mongodb://localhost/dalert'
-module.exports.scdm_url = 'mongodb://localhost/sc_dm'
+module.exports.scdm_url = 'mongodb://localhost/sc_dm'、
+//aaaaa
